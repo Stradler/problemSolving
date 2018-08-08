d6833ebfe5ccb88fccdfb0424a8589c1cc57cf4a
stradler <naykan42@gmail.com>
2018-08-08 11:39:53 +0300

    solve exerciese 2

--- eloquentJS/ch4/exercises.js
+++ eloquentJS/ch4/exercises.js
@@ -20,3 +20,47 @@ function sum(arr, acc = 0, index = 0){
 
   return acc + sum(arr, arr[index], index + 1);
 }
+
+//EXERCIES 2
+
+function reverseArray(arr){
+    const arrayCopy = Array.from(arr);
+    for(let i = 0, n = arrayCopy.length; i < n / 2; i++){
+        [arrayCopy[i], arrayCopy[n - 1 - i]] = [arrayCopy[n - 1 - i], arrayCopy[i]];
+    }
+    return arrayCopy;
+}
+
+function reverseArrayInPlace(arr){
+    for(let i = 0, n = arr.length; i < n / 2; i++){
+        [arr[i], arr[n - 1 - i]] = [arr[n - 1 - i], arr[i]];
+    }
+    return arr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
